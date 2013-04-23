a635e809d0d55da30036ff6deeef90ba0b69b7f7
Yasser Souri <yassersouri@gmail.com>
2013-04-23 19:06:49 +0430

    make it compalitble with pelican 3.1.1

--- jalali.py
+++ jalali.py
@@ -2,9 +2,11 @@ from pelican import signals
 import khayyam
 
 
-def jalali(content):
-    content.locale_date = khayyam.JalaliDatetime.from_datetime(content.date).strftime(content.date_format)
+def jalali(article_generator):
+    # content.locale_date = khayyam.JalaliDatetime.from_datetime(content.date).strftime(content.date_format)
+    for article in article_generator.dates:
+        article.locale_date = khayyam.JalaliDatetime.from_datetime(article.date).strftime(article.date_format)
 
 
 def register():
-    signals.content_object_init.connect(jalali)
+    signals.article_generator_finalized.connect(jalali)
