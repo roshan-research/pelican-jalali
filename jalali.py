95718ff7a0010cb2b04ea9374c308d78bab630b3
Yasser Souri <yassersouri@gmail.com>
2013-04-23 18:07:59 +0430

    fix the plugin
    now everything works

--- jalali.py
+++ jalali.py
@@ -1,12 +1,10 @@
 from pelican import signals
 import khayyam
-from datetime import datetime as dt
+
 
 def jalali(content):
-    content.date = khayyam.JalaliDatetime.from_datetime(content.date)
-    content.date = dt.now()
-    print content.date
-    print '-------------------'
+    content.locale_date = khayyam.JalaliDatetime.from_datetime(content.date).strftime(content.date_format)
+
 
 def register():
     signals.content_object_init.connect(jalali)
