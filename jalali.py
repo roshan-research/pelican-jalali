e02552815665ef843262fb966311cc6e908f2b03
Yasser Souri <yassersouri@gmail.com>
2013-04-24 16:23:44 +0430

    use the patched version that now suppots correct urls

--- jalali.py
+++ jalali.py
@@ -1,11 +1,11 @@
 from pelican import signals
-import khayyam
+import jdatetime
 
 
 def jalali(article_generator):
-    # content.locale_date = khayyam.JalaliDatetime.from_datetime(content.date).strftime(content.date_format)
     for article in article_generator.dates:
-        article.locale_date = khayyam.JalaliDatetime.from_datetime(article.date).strftime(article.date_format)
+        article.locale_date = jdatetime.datetime.fromgregorian(force_persian_output=True, datetime=article.date).strftime(article.date_format)
+        article.date = jdatetime.datetime.fromgregorian(force_persian_output=False, datetime=article.date)
 
 
 def register():
