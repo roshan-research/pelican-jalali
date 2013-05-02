2cee435e0c92090ed3a3bed674c5a627b62466f7
Yasser Souri <yassersouri@gmail.com>
2013-05-02 21:52:58 +0430

    make feed work, temporary

--- jalali.py
+++ jalali.py
@@ -5,7 +5,7 @@ import jdatetime
 def jalali(article_generator):
     for article in article_generator.dates:
         article.locale_date = jdatetime.datetime.fromgregorian(force_persian_output=True, datetime=article.date).strftime(article.date_format)
-        article.date = jdatetime.datetime.fromgregorian(force_persian_output=False, datetime=article.date)
+        # article.date = jdatetime.datetime.fromgregorian(force_persian_output=False, datetime=article.date)
 
 
 def register():
