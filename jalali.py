fa5e51f246f960365663e07a946702e8cdaeb133
Alireza Nourian <alireza.nournia@gmail.com>
2013-12-12 20:20:53 +0330

    add jdatetime content

--- jalali.py
+++ jalali.py
@@ -1,12 +1,11 @@
 from pelican import signals
-import sjdatetime
+import jdatetime
 
 
 def jalali(article_generator):
-    for article in article_generator.dates:
-        article.locale_date = sjdatetime.datetime.fromgregorian(force_persian_output=True, datetime=article.date).strftime(article.date_format)
-        # article.date = sjdatetime.datetime.fromgregorian(force_persian_output=False, datetime=article.date)
+	for article in article_generator.dates:
+		article.locale_date = jdatetime.datetime.fromgregorian(force_persian_output=True, datetime=article.date).strftime(article.date_format)
 
 
 def register():
-    signals.article_generator_finalized.connect(jalali)
+	signals.article_generator_finalized.connect(jalali)
